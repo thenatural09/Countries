d8e5e717d9bf5f66d70893951960322e243318a8
Troy <tward4@tulane.edu>
2016-09-23 13:34:55 -0400

    Array and hashmap

--- src/CountryWrapper.java
+++ src/CountryWrapper.java
@@ -1,5 +1,27 @@
+import com.company.Country;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.ArrayList;
+
 /**
  * Created by Troy on 9/23/16.
  */
 public class CountryWrapper {
+    ArrayList<Country> countries;
+
+    public CountryWrapper() {
+    }
+
+    public CountryWrapper(ArrayList<Country> people) {
+        this.countries = countries;
+    }
+
+    public ArrayList<Country> getPeople() {
+        return countries;
+    }
+
+    public void setPeople(ArrayList<Country> people) {
+        this.countries = countries;
+    }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -11,11 +16,6 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         HashMap<String, ArrayList<Country>> countryMap = new HashMap<>();
         ArrayList<Country> countries = new ArrayList<>();
-
-        //Asking user for country first letter
-
-
-
 
         //File Reader
         File f = new File("countries.txt");
@@ -29,14 +29,45 @@ public class Main {
             countries.add(c);
         }
 
-        //Getting countries by first letter
-        for (int index = 0; index < countries.size(); index++) {
-
+        //Adding array lists to hashmap and making new txt files for each user
+        System.out.println("Enter a letter");
+        Scanner scanner = new Scanner(System.in);
+        for(Country country : countries) {
+            ArrayList<Country> countries1 = countryMap.get(country.name.substring(0,1));
+            if (countries1 == null) {
+                countries1 = new ArrayList<>();
+                countryMap.put(country.name.substring(0,1),countries1);
+            }
+            countries1.add(country);
         }
     }
 
-    //Parsing File
-    public void parseFile(String fileName) {
+    //Loading File
+    public void loadFile(String fileName) {
+        File f = new File(fileName);
+        FileReader fr = null;
+        try {
+            fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] contents = new char[fileSize];
+            fr.read(contents, 0, fileSize);
+            JsonParser parser = new JsonParser();
+        } catch (Exception e) {
+            System.out.println("Couldn't load file");
+        }
+    }
 
+    //Saving File
+    public void saveFile(Country country,String fileName) {
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.deep(true).serialize(country);
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(json);
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Couldn't save to file");
+        }
     }
 }
